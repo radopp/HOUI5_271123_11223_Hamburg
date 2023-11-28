9fbb843a91651c632a96be8d2c787599e8dab4a2
radopp <88769940+radopp@users.noreply.github.com>
2023-11-28 11:13:53 +0100

    Delete webapp directory

--- webapp/test/integration/opaTests.qunit.js
+++
@@ -1,7 +0,0 @@
-/* global QUnit */
-
-sap.ui.require(["at/clouddna/training00/zhoui5/test/integration/AllJourneys"
-], function () {
-	QUnit.config.autostart = false;
-	QUnit.start();
-});
--- webapp/test/unit/AllTests.js
+++
@@ -1,5 +0,0 @@
-sap.ui.define([
-	"atclouddnatraining00/zhoui5/test/unit/controller/Main.controller"
-], function () {
-	"use strict";
-});
